b27598650cd3531b786833ab5bc3ffccde2bccea
Piyush Kumar <68659006+PiyushKumar-18-AI@users.noreply.github.com>
2023-01-08 22:58:08 +0530

    141. Linked List Cycle

--- Data Structure Study Plan-1 LeetCode/Linked List Cycle.cpp
+++ Data Structure Study Plan-1 LeetCode/Linked List Cycle.cpp
@@ -2,15 +2,16 @@
 
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if(l1 == nullptr) return l2;
-        if(l2 == nullptr) return l1;
-        if(l1->val <= l2->val){
-            l1->next = mergeTwoLists(l1->next, l2);
-            return l1;
-        }else{
-            l2->next = mergeTwoLists(l2->next, l1);
-            return l2;
+    bool hasCycle(ListNode *head) {
+        if(head == NULL || head->next == NULL) return false;
+        ListNode *slow = head, *fast = head->next;
+        //if there is a cycle, fast will eventually catch up slow
+        while(slow != fast){
+            if(fast == NULL || fast->next == NULL)
+                return false;
+            slow = slow->next;
+            fast = fast->next->next;
         }
+        return true;
     }
 };
