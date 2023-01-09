7b7a35d68274b3d7745490d91236d99d6b48b2ab
Piyush Kumar <68659006+PiyushKumar-18-AI@users.noreply.github.com>
2023-01-09 12:10:00 +0530

    20. Valid Parentheses

---
+++ Data Structure Study Plan-1 LeetCode/Valid Parentheses.cpp
@@ -0,0 +1,28 @@
+## 20. Valid Parentheses
+
+
+class Solution {
+public:
+    bool isValid(string s) {
+        map<char, char> paren;
+        paren['('] = ')';
+        paren['{'] = '}';
+        paren['['] = ']';
+        
+        set<char> keys = {'(', '{', '['};
+        
+        stack<char> stk;
+        
+        for(char c : s){
+            if(keys.find(c) != keys.end()){//left
+                stk.push(c);
+            }else if(!stk.empty() && paren[stk.top()] == c){//right, match
+                stk.pop();
+            }else{//right, not match
+                return false;
+            }
+        }
+        
+        return stk.empty();
+    }
+};
