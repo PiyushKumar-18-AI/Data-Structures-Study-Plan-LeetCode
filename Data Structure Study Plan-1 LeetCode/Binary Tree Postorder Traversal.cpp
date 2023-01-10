070b4bc356905c32347851fe0beaa2e119b89c9f
Piyush Kumar <68659006+PiyushKumar-18-AI@users.noreply.github.com>
2023-01-10 11:38:45 +0530

    145. Binary Tree Postorder Traversal

---
+++ Data Structure Study Plan-1 LeetCode/Binary Tree Postorder Traversal.cpp
@@ -0,0 +1,18 @@
+## 145. Binary Tree Postorder Traversal
+
+class Solution {
+public:
+    vector<int> ans;
+    
+    void postorder(TreeNode* node){
+        if(!node) return;
+        postorder(node->left);
+        postorder(node->right);
+        ans.push_back(node->val);
+    }
+    
+    vector<int> postorderTraversal(TreeNode* root) {
+        postorder(root);
+        return ans;
+    }
+};
