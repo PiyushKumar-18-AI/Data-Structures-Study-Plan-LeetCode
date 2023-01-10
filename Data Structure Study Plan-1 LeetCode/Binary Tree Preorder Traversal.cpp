af4af147a978f87832d625705da1983941a53bc5
Piyush Kumar <68659006+PiyushKumar-18-AI@users.noreply.github.com>
2023-01-10 11:40:20 +0530

    144. Binary Tree Preorder Traversal

---
+++ Data Structure Study Plan-1 LeetCode/Binary Tree Preorder Traversal.cpp
@@ -0,0 +1,18 @@
+## 144. Binary Tree Preorder Traversal
+
+class Solution {
+public:
+    vector<int> ans;
+    
+    void preorder(TreeNode* node){
+        if(!node) return;
+        ans.push_back(node->val);
+        preorder(node->left);
+        preorder(node->right);
+    }
+    
+    vector<int> preorderTraversal(TreeNode* root) {
+        preorder(root);
+        return ans;
+    }
+};
