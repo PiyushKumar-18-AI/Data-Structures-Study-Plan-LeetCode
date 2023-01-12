2083c4e94eb1ae0ba029ec357c51c54e0c2fa41b
Piyush Kumar <68659006+PiyushKumar-18-AI@users.noreply.github.com>
2023-01-13 00:00:14 +0530

    226. Invert Binary Tree

---
+++ Data Structure Study Plan-1 LeetCode/Invert Binary Tree.cpp
@@ -0,0 +1,13 @@
+## 226. Invert Binary Tree
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root != nullptr){
+            swap(root->left, root->right);
+            root->left = invertTree(root->left);
+            root->right = invertTree(root->right);
+        }
+        return root;
+    }
+};
