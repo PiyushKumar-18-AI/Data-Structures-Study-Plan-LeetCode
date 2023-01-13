8b9cbe03ea9a44a8660373b7db4ac60baef9f573
Piyush Kumar <68659006+PiyushKumar-18-AI@users.noreply.github.com>
2023-01-13 23:51:03 +0530

    700. Search in a Binary Search Tree

---
+++ Data Structure Study Plan-1 LeetCode/Search in a Binary Search Tree.cpp
@@ -0,0 +1,16 @@
+## 700. Search in a Binary Search Tree
+
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root==NULL){
+            return NULL;
+        }else if(root->val==val){
+            return root;
+        }else if(val < root->val){
+            return searchBST(root->left, val);
+        }else{
+            return searchBST(root->right, val);
+        }
+    }
+};
