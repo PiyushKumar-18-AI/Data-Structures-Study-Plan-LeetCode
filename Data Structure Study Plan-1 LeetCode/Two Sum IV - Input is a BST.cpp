5f38618a6d2da64aa292e26de713fb286fc8f69a
Piyush Kumar <68659006+PiyushKumar-18-AI@users.noreply.github.com>
2023-01-14 23:55:15 +0530

    653. Two Sum IV - Input is a BST

---
+++ Data Structure Study Plan-1 LeetCode/Two Sum IV - Input is a BST.cpp
@@ -0,0 +1,29 @@
+## 653. Two Sum IV - Input is a BST
+
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        queue<TreeNode*> q;
+        vector<int> v; //values seen
+        TreeNode* cur;
+        
+        q.push(root);
+        
+        while(!q.empty()){
+            cur = q.front();
+            q.pop();
+            
+            for(int e : v){
+                if(e+cur->val==k){
+                    return true;
+                }
+            }
+            v.push_back(cur->val);
+            
+            if(cur->left) q.push(cur->left);
+            if(cur->right) q.push(cur->right);
+        }
+        
+        return false;
+    }
+};
